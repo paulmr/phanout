b3c7b9fe684387795e55f3b4cdce2bbd1884f82a
Paul Roberts <paul.roberts@guardian.co.uk>
2018-11-28 12:38:31 +0000

    generate the ophan url to lookup

--- build.sbt
+++ build.sbt
@@ -1,5 +1,7 @@
 enablePlugins(ScalaJSPlugin)
 
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.6"
+
 name := "PhanOut"
 scalaVersion := "2.12.6" // or any other Scala version >= 2.10.2
 
--- src/main/scala/phanout.scala
+++ src/main/scala/phanout.scala
@@ -1,7 +1,55 @@
 package gu.com.phanout
 
+import org.scalajs.dom.experimental.URL
+import scala.scalajs.js.URIUtils.encodeURIComponent
+import org.scalajs.dom._
+
 object Phanout {
+
+  def makeOphanUrl(path: String) =
+    s"https://dashboard.ophan.co.uk/info/json?path=${encodeURIComponent(path)}"
+
+  private def log(s: String) = println(s"[PMR] $s")
+
+  def ophanLookup(url: String) = {
+    val path = new URL(url).pathname
+    val xhr = new XMLHttpRequest()
+    log(makeOphanUrl(path))
+    // xhr.open("GET", )
+  }
+
+  def getGuardianLinks() = {
+    val nodes =
+      nodeListIterator(
+        document.querySelectorAll(
+          Seq(
+            """a[href^="https://www.theguardian.com"]""",
+            """a[href^="https://www.guardian.co.uk"]""",
+            """a[href^="http://www.theguardian.com"]""",
+            """a[href^="http://www.guardian.co.uk"]"""
+          ).mkString(",")
+        )
+      )
+    (nodes.collect {
+      case a: html.Anchor => a
+    }).toList
+  }
+
+  def nodeListIterator(nodes: NodeList) = new Iterator[Node] {
+    private var index = 0
+    def hasNext = index < nodes.length
+    def next() = {
+      val res = nodes(index)
+      index = index + 1
+      res
+    }
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Hello world! (phanout)")
+    val links = getGuardianLinks()
+    /* now for each guardian link, we want to lookup the URL in Ophan */
+    for(l <- links) {
+      ophanLookup(l.href)
+    }
   }
 }
